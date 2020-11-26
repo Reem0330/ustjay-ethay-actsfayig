efb21e691ddbf48f6d153ce8bacfbeee43d3d20d
Reem0330 <53073163+Reem0330@users.noreply.github.com>
2020-11-25 18:10:00 -0800

    mashup a random fact from http://unkno.com

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+
+import requests
+from flask import Flask, send_file, Response
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
+
+def get_fact():
+
+    response = requests.get("http://unkno.com")
+
+    soup = BeautifulSoup(response.content, "html.parser")
+    facts = soup.find_all("div", id="content")
+
+    return facts[0].getText()
+
+def get_pig_latin(fact):
+    payload = {'input_text': fact}
+    response = requests.post('https://hidden-journey-62459.herokuapp.com/piglatinize/', allow_redirects=False, data=payload)
+
+    return response.headers['Location']
+
+@app.route('/')
+def home():
+    fact = get_fact().strip()
+    body = get_pig_latin(fact)
+
+    return Response(response=body)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 6787))
+    app.run(host='127.0.0.1', port=port)
+
